898e1666714bfdc8e03c7dcd015b2f9d4e962a6d
osvaldosilitonga <osvaldosilitonga@gmail.com>
2023-09-27 15:43:45 +0700

    Update - 'log-analyzer.go' file

--- log/log-analyzer.go
+++ log/log-analyzer.go
@@ -1,4 +1,4 @@
-package log
+package main
 
 import (
 	"bufio"
@@ -8,28 +8,43 @@ import (
 )
 
 func main() {
+	// Recover Panic
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Error: ", r)
 		}
 	}()
 
-	// Open File
-	file, err := os.Open("data.txt")
+	// Open File 'log.txt'
+	file, err := os.Open("log.txt")
 	if err != nil {
 		panic("Tidak dapat membuka file 'log.txt'")
 	}
-
-	defer file.Close()
+	defer file.Close() // 'log.txt' file akan di close setelah proses selesai
 
 	scanner := bufio.NewScanner(file)
 
-	// data := []string{}
-
+	// Scan 'log.txt'
+	var infoCount, errCount, debugCount int
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.Contains(line, "[ERROR]") {
+			errCount++
+			continue
+		}
+		if strings.Contains(line, "[DEBUG]") {
+			debugCount++
+			continue
+		}
+		if strings.Contains(line, "[INFO]") {
+			infoCount++
+			continue
 		}
 	}
+
+	// Display Output
+	fmt.Printf("[INFO] Level : %d occurrences\n", infoCount)
+	fmt.Printf("[ERROR] Level : %d occurrences\n", errCount)
+	fmt.Printf("[DEBUG] Level : %d occurrences\n", debugCount)
 
 }
